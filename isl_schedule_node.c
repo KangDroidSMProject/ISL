f95531f78b5aa3f25fa30b90530466ea813c6d75
Paul Beeler <pbeeler80@gmail.com>
2015-06-16 00:24:50 -0600

    Revert "rename isl_schedule_*_foreach_* to isl_schedule_*_foreach_*_top_down"
    This reverts commit 5482bda29c45b1bfc5238b663a867bdc3059f999.

--- isl_schedule_node.c
+++ isl_schedule_node.c
@@ -1288,7 +1288,7 @@ static __isl_give isl_schedule_node *traverse(
 	return node;
 }
 
-/* Internal data structure for isl_schedule_node_foreach_descendant_top_down.
+/* Internal data structure for isl_schedule_node_foreach_descendant.
  *
  * "fn" is the user-specified callback function.
  * "user" is the user-specified argument for the callback.
@@ -1350,7 +1350,7 @@ static __isl_give isl_schedule_node *preorder_leave(
  *
  * Return 0 on success and -1 on failure.
  */
-isl_stat isl_schedule_node_foreach_descendant_top_down(
+isl_stat isl_schedule_node_foreach_descendant(
 	__isl_keep isl_schedule_node *node,
 	isl_bool (*fn)(__isl_keep isl_schedule_node *node, void *user),
 	void *user)
